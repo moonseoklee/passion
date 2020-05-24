f0d3190fd702e3ea4eb737a70666f0b9f92f216a
winnyiee@naver.com <winnyiee@naver.com>
2020-05-25 01:37:04 +0900

    first

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    
+    //daf
   });
 
 const PORT = process.env.PORT || 5000;
