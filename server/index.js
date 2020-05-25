ceb1954ae795ec065dd00379521e73aea1f13dc8
winnyiee@naver.com <winnyiee@naver.com>
2020-05-25 16:13:05 +0900

    add parcel

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "client/dist")));
   }
 
 app.use(express.static('dist'))
